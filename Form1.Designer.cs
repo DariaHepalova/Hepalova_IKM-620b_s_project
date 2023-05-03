9f491bacf29b04094f3d0fedc92aa1300077c3e5
HP <HP@LAPTOP-ELVF8Q07>
2023-05-03 09:52:38 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Hepalova_IKM_620b_s_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            bStart = new Button();
+            label1 = new Label();
+            tbInput = new TextBox();
+            tClock = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // bStart
+            // 
+            bStart.Location = new Point(305, 221);
+            bStart.Name = "bStart";
+            bStart.Size = new Size(75, 23);
+            bStart.TabIndex = 0;
+            bStart.Text = "Пуск";
+            bStart.UseVisualStyleBackColor = true;
+            bStart.Click += bStart_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(305, 104);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // tbInput
+            // 
+            tbInput.Enabled = false;
+            tbInput.Location = new Point(305, 161);
+            tbInput.Name = "tbInput";
+            tbInput.Size = new Size(100, 23);
+            tbInput.TabIndex = 2;
+            tbInput.KeyPress += tbInput_KeyPress;
+            // 
+            // tClock
+            // 
+            tClock.Interval = 25000;
+            tClock.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tbInput);
+            Controls.Add(label1);
+            Controls.Add(bStart);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button bStart;
+        private Label label1;
+        private TextBox tbInput;
+        private System.Windows.Forms.Timer tClock;
+    }
+}
